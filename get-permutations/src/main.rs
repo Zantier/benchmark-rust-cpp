cb0274d9aa2a6b5fe513422d6e1a8c8e63be13b4
David Phillips <zantier.tasa@gmail.com>
2021-12-07 14:29:43 +0000

    Get Permutations: In rust, use Vec<u8> rather than String

--- get-permutations/src/main.rs
+++ get-permutations/src/main.rs
@@ -1,9 +1,9 @@
 const N: i32 = 10;
 struct Perm {
-    res: Vec<String>,
+    res: Vec<Vec<u8>>,
 }
 impl Perm {
-    fn rec(&mut self, done: i32, s: String) {
+    fn rec(&mut self, done: i32, s: Vec<u8>) {
         if done == (1<<N)-1 {
             self.res.push(s);
             return;
@@ -14,8 +14,9 @@ impl Perm {
                 continue;
             }
 
-            let c = char::from_u32(b'A' as u32 + i as u32).unwrap();
-            let mut s2 = s.clone();
+            let c = b'A' + i as u8;
+            let mut s2 = Vec::with_capacity(s.len()+1);
+            s2.extend_from_slice(&s);
             s2.push(c);
             self.rec(done|1<<i, s2);
         }
@@ -26,9 +27,9 @@ fn main() {
         res: Vec::new(),
     };
 
-    perm.rec(0, String::from(""));
+    perm.rec(0, Vec::new());
     println!("Size: {}", perm.res.len());
     for s in perm.res.iter().take(5) {
-        println!("{}", s);
+        println!("{:?}", s);
     }
 }
