8196497c8c57c2879a3bb2c9098709391fd89d1d
Owen Shepherd <owen@owen.cafe>
2021-12-07 14:08:22 +0000

    less mallocs

--- get-permutations/src/main.rs
+++ get-permutations/src/main.rs
@@ -2,7 +2,23 @@ const N: i32 = 10;
 struct Perm {
     res: Vec<String>,
 }
+
+fn fact(n: usize) -> usize {
+    let mut res = 1;
+    for i in 2..n+1 {
+        res *= i;
+    }
+    res
+}
+
 impl Perm {
+
+    fn new(n: i32) -> Perm {
+        Perm {
+            res: Vec::with_capacity(fact(n as usize))
+        }
+    }
+
     fn rec(&mut self, done: i32, s: String) {
         if done == (1<<N)-1 {
             self.res.push(s);
@@ -15,16 +31,15 @@ impl Perm {
             }
 
             let c = char::from_u32(b'A' as u32 + i as u32).unwrap();
-            let mut s2 = s.clone();
+            let mut s2 = String::with_capacity(s.len() + 1);
+            s2.push_str(&s);
             s2.push(c);
             self.rec(done|1<<i, s2);
         }
     }
 }
 fn main() {
-    let mut perm = Perm {
-        res: Vec::new(),
-    };
+    let mut perm = Perm::new(N);
 
     perm.rec(0, String::from(""));
     println!("Size: {}", perm.res.len());
